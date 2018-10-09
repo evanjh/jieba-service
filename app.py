35c0361105da086f32294f70c10cf9dc9d4bc1b6
Lukin <luxiansheng@mogoroom.com>
2018-10-09 18:58:53 +0800

    update

--- app.py
+++ app.py
@@ -10,7 +10,37 @@ sys.path.append('../')
 app = Flask(__name__)
 
 USER_DICT_LOADED = False
-JIANHUANG_LOADED = False
+from keras.preprocessing.image import img_to_array, load_img
+from keras.layers.core import Dense, Flatten
+from keras.layers import Input
+from keras.models import Model
+from keras import optimizers
+import keras as ks
+
+# input image dimensions
+img_rows, img_cols = 128, 128
+img_channels = 3
+n_classes = 1
+
+# define model
+img_input = Input(shape=(img_rows, img_cols, 3))
+xinception = ks.applications.Xception(include_top=False, weights=None, input_tensor=img_input)
+output = xinception.output
+output = Flatten(name='flatten')(output)
+output = Dense(n_classes, activation='sigmoid', name='predictions')(output)
+model = Model(xinception.input, output)
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.Adam(),
+              metrics=["accuracy"])
+
+# load weights
+if os.path.exists('./model/0.12-loss_18epoch_128x128_aug_0.001lr_run0_Xception_128_1493071505.11time.h5'):
+    print('loading weights..........')
+    model.load_weights('./model/0.12-loss_18epoch_128x128_aug_0.001lr_run0_Xception_128_1493071505.11time.h5')
+    JIANHUANG_LOADED = True
+    print('load OK!')
+else:
+    print('Not Find Weights........')
 
 
 @app.route('/jieba')
@@ -42,44 +72,12 @@ def index():
 
 @app.route('/jianhuang')
 def jianhuang():
-    global JIANHUANG_LOADED
     if request.method == 'POST':
         path = request.form.get('path')
     else:
         path = request.args.get('path')
 
-    from keras.preprocessing.image import img_to_array, load_img
-    from keras.layers.core import Dense, Flatten
-    from keras.layers import Input
-    from keras.models import Model
-    from keras import optimizers
-    import keras as ks
 
-    # input image dimensions
-    img_rows, img_cols = 128, 128
-    img_channels = 3
-    n_classes = 1
-
-    # define model
-    img_input = Input(shape=(img_rows, img_cols, 3))
-    xinception = ks.applications.Xception(include_top=False, weights=None, input_tensor=img_input)
-    output = xinception.output
-    output = Flatten(name='flatten')(output)
-    output = Dense(n_classes, activation='sigmoid', name='predictions')(output)
-    model = Model(xinception.input, output)
-    model.compile(loss='binary_crossentropy',
-                  optimizer=optimizers.Adam(),
-                  metrics=["accuracy"])
-
-    # load weights
-    if False == JIANHUANG_LOADED:
-        if os.path.exists('./model/0.12-loss_18epoch_128x128_aug_0.001lr_run0_Xception_128_1493071505.11time.h5'):
-            print('loading weights..........')
-            model.load_weights('./model/0.12-loss_18epoch_128x128_aug_0.001lr_run0_Xception_128_1493071505.11time.h5')
-            JIANHUANG_LOADED = True
-            print('load OK!')
-        else:
-            print('Not Find Weights........')
 
     re_img = load_img(path)  # this is a jpg image
     re_img = re_img.resize((img_cols, img_rows))
